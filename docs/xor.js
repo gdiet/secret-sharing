8e072714b1e68b621b2610111f0f1930918e382e
georg <gdiet@users.noreply.github.com>
2023-03-23 23:11:22 +0100

    fill (K-1) lists with random bytes

--- docs/xor.js
+++ docs/xor.js
@@ -10,11 +10,32 @@ let listen = (func, id, eventType, listener) => func(id).addEventListener(eventT
 // text utility functions
 let fromUTF8 = (text) => Array.from(new TextEncoder().encode(text));
 let toUTF8 = (bytes) => new TextDecoder().decode(new Uint8Array(bytes));
+let parseInts = (text, ifParseFails) => text && text.split(',').map(n => parseInt(n) || ifParseFails) || Array();
+// numeric utility functions - min is inclusive and max is exclusive
+let random = (min, max) => Math.floor(Math.random() * (max - min) + min);
+let limit = (n, min, max) => Math.min(max - 1, Math.max(min, n));
+// document content
+let cont = {
+    get secretText() { return input('secretTextInput').value; },
+    get secretBytes() { return parseInts(input('secretBytesInput').value, 95); },
+    get shareNumber() { return parseInt(input('shareNumberInput').value); },
+    set shareNumber(k) { input('shareNumberInput').value = String(k); },
+    shareInput: (index) => input(`share${index}Input`),
+};
 // document automation
 listen(button, 'textToBytesButton', 'click', () => {
-    input('numbersInput').value = fromUTF8(input('textInput').value).join(',');
+    input('secretBytesInput').value = fromUTF8(cont.secretText).join(',');
 });
 listen(button, 'bytesToTextButton', 'click', () => {
-    input('textInput').value = toUTF8(input('numbersInput').value.split(',').map(n => parseInt(n) || 95));
+    input('secretTextInput').value = toUTF8(cont.secretBytes);
+});
+listen(input, 'shareNumberInput', 'change', () => {
+    cont.shareNumber = limit(cont.shareNumber, 2, 7);
+    for (let i = 1; i <= 5; i++)
+        input(`share${i}Input`).hidden = i >= cont.shareNumber;
+});
+listen(button, 'fillWithRandomBytesButton', 'click', () => {
+    for (let i = 1; i < cont.shareNumber; i++)
+        cont.shareInput(i).value = cont.secretBytes.map(() => random(0, 256)).join(",");
 });
 //# sourceMappingURL=xor.js.map
--- xor.ts
+++ xor.ts
@@ -8,13 +8,36 @@ let paragraph = (id: string) => document.getElementById(id) as HTMLParagraphElem
 let listen    = (func: (id: string) => HTMLElement, id: string, eventType: string, listener: EventListenerOrEventListenerObject) => func(id).addEventListener(eventType, listener)
 
 // text utility functions
-let fromUTF8 = (text: string)    => Array.from(new TextEncoder().encode(text))
-let toUTF8   = (bytes: number[]) => new TextDecoder().decode(new Uint8Array(bytes))
+let fromUTF8 = (text: string) => Array.from(new TextEncoder().encode(text))
+let toUTF8 = (bytes: number[]) => new TextDecoder().decode(new Uint8Array(bytes))
+let parseInts = (text: string, ifParseFails: number) => text && text.split(',').map(n => parseInt(n) || ifParseFails) || Array<number>()
+
+// numeric utility functions - min is inclusive and max is exclusive
+let random = (min: number, max: number) => Math.floor(Math.random() * (max - min) + min)
+let limit = (n: number, min: number, max: number) => Math.min(max - 1, Math.max(min, n))
+
+// document content
+let cont = {
+  get secretText() { return input('secretTextInput').value },
+  get secretBytes() { return parseInts(input('secretBytesInput').value, 95) },
+  get shareNumber() { return parseInt(input('shareNumberInput').value) },
+  set shareNumber(k: number) { input('shareNumberInput').value = String(k) },
+  shareInput: (index: number) => input(`share${index}Input`),
+}
 
 // document automation
 listen(button, 'textToBytesButton', 'click', () => {
-  input('numbersInput').value = fromUTF8(input('textInput').value).join(',')
+  input('secretBytesInput').value = fromUTF8(cont.secretText).join(',')
 })
 listen(button, 'bytesToTextButton', 'click', () => {
-  input('textInput').value = toUTF8(input('numbersInput').value.split(',').map(n => parseInt(n) || 95))
+  input('secretTextInput').value = toUTF8(cont.secretBytes)
+})
+listen(input, 'shareNumberInput', 'change', () => {
+  cont.shareNumber = limit(cont.shareNumber, 2, 7)
+  for (let i = 1; i <= 5; i++)
+    input(`share${i}Input`).hidden = i >= cont.shareNumber
+})
+listen(button, 'fillWithRandomBytesButton', 'click', () => {
+  for (let i = 1; i < cont.shareNumber; i++)
+    cont.shareInput(i).value = cont.secretBytes.map(() => random(0, 256)).join(",")
 })
