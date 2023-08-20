a0cb7ef44628300bce063a4e8c08883052eda115
georg <gdiet@users.noreply.github.com>
2023-08-20 19:45:18 +0200

    cleanup - first complete AES GF(256) SSS implementation

--- scala/src/main/scala/shamir/main.scala
+++ scala/src/main/scala/shamir/main.scala
@@ -1,22 +1,23 @@
 package shamir
 
-/** This is an implementation of Shamir's secret sharing using the same Galois field GF(256) that is used by the AES
-  * encryption, that is, the `x^8 + x^4 + x^3 + x^1 + x^0` polynomial, with the same big-endian bit order for bytes.
+/** Shamir's secret sharing implemented using the Galois field GF(256) used by the AES encryption,
+  * the `x^8 + x^4 + x^3 + x^1 + x^0` polynomial, with big-endian bit order for bytes.
+  * 
   * For string from/to byte sequence conversions, UTF-8 is used.
   *
   * @see https://en.wikipedia.org/wiki/Shamir%27s_secret_sharing
   * @see https://en.wikipedia.org/wiki/Finite_field_arithmetic#Rijndael's_(AES)_finite_field */
 @main def main(): Unit =
-  val shares = shareSecret(stringToBytes("test"), 4, 2, (_, _) => 5)
-  println(shares.map(bytesToHex).mkString("\n"))
+  val random = scala.util.Random.between(_: Int, _: Int)
+  val shares = shareSecret(toBytes("test"), 4, 2, random)
+  println(shares.map(toHex).mkString("\n"))
 
-  import util.chaining.scalaUtilChainingOps
   val recovered = joinShares(Seq(shares(3), shares(1)))
   println()
-  println(bytesToHex(recovered))
+  println(toHex(recovered))
   println(new String(recovered.map(_.toByte), "UTF-8"))
 
-def stringToBytes(string: String): Array[Int] = string.getBytes("UTF-8").map(byteToInt)
-def bytesToHex(bytes: Array[Int]): String = bytes.map(byteToHex).mkString
-def byteToHex(byte: Int ): String = f"$byte%02x"
-def byteToInt(byte: Byte): Int    = java.lang.Byte.toUnsignedInt(byte)
+def toBytes(string: String    ): Array[Int] = string.getBytes("UTF-8").map(toInt)
+def toInt  (byte  : Byte      ): Int        = java.lang.Byte.toUnsignedInt(byte)
+def toHex  (bytes : Array[Int]): String     = bytes.map(toHex).mkString
+def toHex  (byte  : Int       ): String     = f"$byte%02x"
