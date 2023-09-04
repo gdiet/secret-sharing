b0549d393b6030b1291c0aef19adb7da48e2d08b
georg <gdiet@users.noreply.github.com>
2023-09-04 07:55:54 +0200

    reordered the functions in the order they are used

--- typescript/src/share.ts
+++ typescript/src/share.ts
@@ -21,6 +21,11 @@ function createShares(): void {
 
 // ------------ secret sharing ------------
 
+function shareSecret(secretBytes: number[], numberOfShares: number, threshold: number): number[][] {
+  const polynomials = generatePolynomials(secretBytes, threshold)
+  return createSecrets(numberOfShares, polynomials)
+}
+
 function generatePolynomials(secretBytes: number[], threshold: number): number[][] {
   return secretBytes.map((secretByte) => generatePolynomial(secretByte, threshold))
 }
@@ -33,13 +38,10 @@ function generatePolynomial(firstByte: number, arraySize: number): number[] {
   return polynomial
 }
 
-/** @see https://en.wikipedia.org/wiki/Horner%27s_method */
-function evaluate(polynomial: number[], share: number): number {
-  let result = 0
-  // Note: ES2023 introduces Array.prototype.toReversed() which is the combination of clone + reverse,
-  // see e.g.https://tc39.es/ecma262/2023/
-  for (const coefficient of Array.from(polynomial).reverse()) result = gf256add(gf256mul(result, share), coefficient)
-  return result
+function randomInt(fromInclusive: number, untilExclusive: number) {
+  const fakerandom = parseInt(new URLSearchParams(document.location.search).get('fakerandom') || '0')
+  if (fakerandom != 0) return fakerandom
+  else return Math.floor(Math.random() * (untilExclusive - fromInclusive) + fromInclusive)
 }
 
 function createSecrets(numOfShares: number, polynomials: number[][]): number[][] {
@@ -49,15 +51,13 @@ function createSecrets(numOfShares: number, polynomials: number[][]): number[][]
   return result
 }
 
-function shareSecret(secretBytes: number[], numberOfShares: number, threshold: number): number[][] {
-  const polynomials = generatePolynomials(secretBytes, threshold)
-  return createSecrets(numberOfShares, polynomials)
-}
-
-function randomInt(fromInclusive: number, untilExclusive: number) {
-  const fakerandom = parseInt(new URLSearchParams(document.location.search).get('fakerandom') || '0')
-  if (fakerandom != 0) return fakerandom
-  else return Math.floor(Math.random() * (untilExclusive - fromInclusive) + fromInclusive)
+/** @see https://en.wikipedia.org/wiki/Horner%27s_method */
+function evaluate(polynomial: number[], share: number): number {
+  let result = 0
+  // Note: ES2023 introduces Array.prototype.toReversed() which is the combination of clone + reverse,
+  // see e.g.https://tc39.es/ecma262/2023/
+  for (const coefficient of Array.from(polynomial).reverse()) result = gf256add(gf256mul(result, share), coefficient)
+  return result
 }
 
 // ------------ gf256 math ------------
