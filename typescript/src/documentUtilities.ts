379bc7ed8447ebf38c95b1d62ca0bf55d6617a49
georg <gdiet@users.noreply.github.com>
2023-09-11 09:55:20 +0200

    use namespace for docutils and introduce AssertionFailed error

--- typescript/src/documentUtilities.ts
+++ typescript/src/documentUtilities.ts
@@ -1,32 +1,41 @@
-const docutils = {
-  documentElement(id: string): HTMLElement {
+namespace docutils {
+  export function documentElement(id: string): HTMLElement {
     const maybeElement = document.getElementById(id)
     if (maybeElement !== null) return maybeElement
-    else return docutils.fail('Sorry, null check in script failed.')
-  },
+    else return fail('Sorry, null check in script failed.')
+  }
 
-  inputElement(id: string): HTMLInputElement {
+  export function inputElement(id: string): HTMLInputElement {
     const maybeInput = docutils.documentElement(id)
     if (maybeInput instanceof HTMLInputElement) return maybeInput
-    else return docutils.fail('Sorry, type check in script failed.')
-  },
+    else return fail('Sorry, type check in script failed.')
+  }
 
-  setClassAndContent(id: string, className: string, innerHTML: string) {
+  export function setClassAndContent(id: string, className: string, innerHTML: string) {
     const element = docutils.documentElement(id)
     element.className = className
     element.innerHTML = innerHTML
-  },
+  }
 
-  registerListener(id: string, eventType: string, listener: (event: Event) => void): void {
+  export function registerListener(id: string, eventType: string, listener: (event: Event) => void): void {
     docutils.documentElement(id).addEventListener(eventType, listener)
-  },
+  }
+
+  /**
+   * Don't catch this error, see below 'function fail'.
+   */
+  export class AssertionFailed extends Error {
+    constructor(message: string) {
+      super(message)
+    }
+  }
 
   /**
    * Stops the current execution, displaying an error message. The error thrown here should not be caught.
    * Only use for program failures, not for input validation.
    */ // FIXME review usage
-  fail(message: string): never {
+  export function fail(message: string): never {
     alert(message)
-    throw Error(message)
-  },
+    throw new AssertionFailed(message)
+  }
 }
